4fe915137c7507dc176cda8ed1d40523b3770fe0
nbena <ciaobena@outlook.it>
2018-04-12 15:02:24 +0200

    added new basic APIs

--- api_diff.go
+++ api_diff.go
@@ -17,11 +17,50 @@
 
 package ftp
 
-import (
-	"bytes"
-	"io"
-	"os"
-)
+func (f *Conn) StoreSimple(
+	mode Mode,
+	src,
+	dst string,
+) error {
+	doneChan := make(chan struct{}, 1)
+	abortChan := make(chan struct{}, 1)
+	startingChan := make(chan struct{}, 1)
+	errChan := make(chan error, 1)
+
+	f.internalStore(mode, src, dst, doneChan, abortChan, startingChan,
+		errChan, false)
+
+	var returned error
+
+	select {
+	case <-doneChan:
+	case err := <-errChan:
+		returned = err
+	}
+	return returned
+}
+
+func (f *Conn) RetrSimple(
+	mode Mode,
+	src,
+	dst string,
+) error {
+	doneChan := make(chan struct{}, 1)
+	abortChan := make(chan struct{}, 1)
+	startingChan := make(chan struct{}, 1)
+	errChan := make(chan error, 1)
+
+	f.internalRetr(mode, src, dst, doneChan, abortChan, startingChan, errChan)
+
+	var returned error
+
+	select {
+	case <-doneChan:
+	case err := <-errChan:
+		returned = err
+	}
+	return returned
+}
 
 // Store loads a file to the server. The file is 'filepath',
 // specifies which FTP you want to use,
@@ -73,152 +112,152 @@ func (f *Conn) Store(
 			 indicating that the abort command was successfully
 			 processed.
 	*/
-
-	var sender io.WriteCloser
-
-	// _, fileName := path.Split(src)
-
-	if mode == IndMode {
-		mode = f.config.DefaultMode
-	}
-
-	if mode == ActiveMode {
-
-		listener, err := f.openListener()
-		if err != nil {
-			errChan <- err
-			return
-		}
-		defer listener.Close()
-
-		if _, err = f.writeCommandAndGetResponse("STOR " + dst + "\r\n"); err != nil {
-			errChan <- err
-			return
-		}
-
-		sender, err = listener.Accept()
-		if err != nil {
-			errChan <- err
-			return
-		}
-
-	} else if mode == PassiveMode {
-
-		addr, err := f.pasvGetAddr()
-		if err != nil {
-			errChan <- err
-			return
-		}
-
-		// write command
-		if _, err = f.writeCommandAndGetResponse("STOR " + dst + "\r\n"); err != nil {
-			errChan <- err
-			return
-		}
-
-		sender, err = f.connectToAddr(addr)
-		if err != nil {
-			errChan <- err
-			return
-		}
-
-	}
-
-	var n int
-	file, err := os.Open(src)
-	if err != nil {
-		errChan <- err
-		return
-	}
-
-	info, err := file.Stat()
-	if err != nil {
-		errChan <- err
-		return
-	}
-
-	buffer := make([]byte, 1024)
-
-	// command has been issued, notifying on startingChan
-	startingChan <- struct{}{}
-
-	for n < int(info.Size()) {
-
-		select {
-		// starting polling on abort
-		case <-abortChan:
-			// it's not completely correct to close here the data channel,
-			// but some server will expect the client to do this.
-			sender.Close()
-			response, err := f.writeCommandAndGetResponse("ABOR\r\n")
-			if err != nil {
-				errChan <- err
-				return
-			}
-
-			// SOME SERVER SUCH APACHE WILL RETURN US A 226
-			// EVEN IF NO FILE HAS BEEN TRANSFERED, WHILE,
-			// ACCORDING TO RFC IT'D RETURN US A 426 FOLLOWED BY A 226.
-			// SO IT RETURNS US 226 AND 226.
-
-			// this in theory should be only if == AbortOk
-			if response.Code == AbortOk || response.Code == TransferOk {
-				// after the first response, server must send another with
-				// 226.
-				abortResponse, err := f.getFtpResponse()
-				if err != nil {
-					// very bad
-					errChan <- err
-					return
-				} else if abortResponse.Code != TransferOk {
-					errChan <- newUnexpectedCodeError(TransferOk, abortResponse.Code)
-					return
-				}
-			} else {
-				errChan <- newUnexpectedCodeError(AbortOk, response.Code)
-				return
-			}
-
-			// deleting the file if required.
-			if deleteIfAbort {
-				if _, err := f.DeleteFile(dst); err != nil {
-					errChan <- err
-				}
-			}
-			doneChan <- struct{}{}
-			return
-
-		// when no abort has been received, go on
-		default:
-			// reading from file
-			read, err := file.Read(buffer)
-			if err != nil {
-				sender.Close()
-				errChan <- err
-				return
-			}
-
-			// updating the number of read bytes
-			n += read
-			_, err = sender.Write(buffer[:read])
-			if err != nil {
-				sender.Close()
-				errChan <- err
-				return
-			}
-		}
-	}
-
-	// until I close the data connection it doesn't answer me.
-	sender.Close()
-
-	// when completed reading response.
-	if _, err := f.getFtpResponse(); err != nil {
-		errChan <- err
-		return
-	}
-
-	doneChan <- struct{}{}
+	f.internalStore(mode, src, dst, doneChan, abortChan, startingChan, errChan, deleteIfAbort)
+	// var sender io.WriteCloser
+	//
+	// // _, fileName := path.Split(src)
+	//
+	// if mode == IndMode {
+	// 	mode = f.config.DefaultMode
+	// }
+	//
+	// if mode == ActiveMode {
+	//
+	// 	listener, err := f.openListener()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	// 	defer listener.Close()
+	//
+	// 	if _, err = f.writeCommandAndGetResponse("STOR " + dst + "\r\n"); err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	sender, err = listener.Accept()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// } else if mode == PassiveMode {
+	//
+	// 	addr, err := f.pasvGetAddr()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	// write command
+	// 	if _, err = f.writeCommandAndGetResponse("STOR " + dst + "\r\n"); err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	sender, err = f.connectToAddr(addr)
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// }
+	//
+	// var n int
+	// file, err := os.Open(src)
+	// if err != nil {
+	// 	errChan <- err
+	// 	return
+	// }
+	//
+	// info, err := file.Stat()
+	// if err != nil {
+	// 	errChan <- err
+	// 	return
+	// }
+	//
+	// buffer := make([]byte, 1024)
+	//
+	// // command has been issued, notifying on startingChan
+	// startingChan <- struct{}{}
+	//
+	// for n < int(info.Size()) {
+	//
+	// 	select {
+	// 	// starting polling on abort
+	// 	case <-abortChan:
+	// 		// it's not completely correct to close here the data channel,
+	// 		// but some server will expect the client to do this.
+	// 		sender.Close()
+	// 		response, err := f.writeCommandAndGetResponse("ABOR\r\n")
+	// 		if err != nil {
+	// 			errChan <- err
+	// 			return
+	// 		}
+	//
+	// 		// SOME SERVER SUCH APACHE WILL RETURN US A 226
+	// 		// EVEN IF NO FILE HAS BEEN TRANSFERED, WHILE,
+	// 		// ACCORDING TO RFC IT'D RETURN US A 426 FOLLOWED BY A 226.
+	// 		// SO IT RETURNS US 226 AND 226.
+	//
+	// 		// this in theory should be only if == AbortOk
+	// 		if response.Code == AbortOk || response.Code == TransferOk {
+	// 			// after the first response, server must send another with
+	// 			// 226.
+	// 			abortResponse, err := f.getFtpResponse()
+	// 			if err != nil {
+	// 				// very bad
+	// 				errChan <- err
+	// 				return
+	// 			} else if abortResponse.Code != TransferOk {
+	// 				errChan <- newUnexpectedCodeError(TransferOk, abortResponse.Code)
+	// 				return
+	// 			}
+	// 		} else {
+	// 			errChan <- newUnexpectedCodeError(AbortOk, response.Code)
+	// 			return
+	// 		}
+	//
+	// 		// deleting the file if required.
+	// 		if deleteIfAbort {
+	// 			if _, err := f.DeleteFile(dst); err != nil {
+	// 				errChan <- err
+	// 			}
+	// 		}
+	// 		doneChan <- struct{}{}
+	// 		return
+	//
+	// 	// when no abort has been received, go on
+	// 	default:
+	// 		// reading from file
+	// 		read, err := file.Read(buffer)
+	// 		if err != nil {
+	// 			sender.Close()
+	// 			errChan <- err
+	// 			return
+	// 		}
+	//
+	// 		// updating the number of read bytes
+	// 		n += read
+	// 		_, err = sender.Write(buffer[:read])
+	// 		if err != nil {
+	// 			sender.Close()
+	// 			errChan <- err
+	// 			return
+	// 		}
+	// 	}
+	// }
+	//
+	// // until I close the data connection it doesn't answer me.
+	// sender.Close()
+	//
+	// // when completed reading response.
+	// if _, err := f.getFtpResponse(); err != nil {
+	// 	errChan <- err
+	// 	return
+	// }
+	//
+	// doneChan <- struct{}{}
 }
 
 // Retrieve download a file located at filepathSrc to filepathDest.
@@ -232,148 +271,150 @@ func (f *Conn) Retrieve(mode Mode,
 	startingChan chan<- struct{},
 	errChan chan<- error) {
 
-	var receiver io.ReadCloser
+	f.internalRetr(mode, filepathSrc, filepathDest, doneChan, abortChan, startingChan, errChan)
 
-	if mode == IndMode {
-		mode = f.config.DefaultMode
-	}
-
-	if mode == ActiveMode {
-
-		//opening a listener.
-		listener, err := f.openListener()
-		if err != nil {
-			errChan <- err
-			return
-		}
-		defer listener.Close()
-
-		// sending command.
-		if _, err = f.writeCommandAndGetResponse("RETR " + filepathSrc + "\r\n"); err != nil {
-			errChan <- err
-			return
-		}
-
-		// accept connection.
-		receiver, err = listener.Accept()
-		if err != nil {
-			errChan <- err
-			return
-		}
-	} else if mode == PassiveMode {
-
-		addr, err := f.pasvGetAddr()
-		if err != nil {
-			errChan <- err
-			return
-		}
-
-		// write command
-		if _, err = f.writeCommandAndGetResponse("RETR " + filepathSrc + "\r\n"); err != nil {
-			errChan <- err
-			return
-		}
-
-		receiver, err = f.connectToAddr(addr)
-		if err != nil {
-			errChan <- err
-			return
-		}
-	}
-
-	file, err := os.Create(filepathDest)
-	if err != nil {
-		errChan <- err
-		return
-	}
-
-	// starting reading into receiver
-	buffer := make([]byte, 1024)
-	loop := true
-
-	// command has been issued, notify on startingChan
-	startingChan <- struct{}{}
-
-	for loop {
-
-		select {
-
-		case <-abortChan:
-			receiver.Close()
-			// var response *Response //declaring here just to prevent go vet.
-			response, err := f.writeCommandAndGetResponse("ABOR\r\n")
-			if err != nil {
-				errChan <- err
-
-				os.Remove(file.Name()) //skipping the error.
-				return
-			}
-
-			// if 426 we have to wait for another response.
-			// if 226 the transfer is complete
-			if response.Code == AbortOk || response.Code == TransferOk {
-				// ok, wait for another.
-				abortResponse, err := f.getFtpResponse()
-				if err != nil {
-					// very bad
-					errChan <- err
-					return
-				} else if abortResponse.Code != TransferOk {
-					errChan <- newUnexpectedCodeError(TransferOk, abortResponse.Code)
-					return
-				}
-			} else {
-				errChan <- newUnexpectedCodeError(AbortOk, response.Code)
-				return
-			}
-
-			err = os.Remove(file.Name())
-			if err != nil {
-				errChan <- err
-			} else {
-				doneChan <- struct{}{}
-			}
-			return
-
-		default:
-
-			_, err = receiver.Read(buffer)
-			if err != nil && err == io.EOF {
-				// I know that it is ugly
-				// but in this way we can skip some if.
-				// EOF means the connection has been closed.
-				loop = false
-				continue // before there was a break but it'll make
-				// exiting just from the select.
-			} else if err != nil {
-				receiver.Close()
-				errChan <- err
-				return
-			}
-
-			// so we can skip null bytes that are added
-			/// to fill the buffer size.
-			index := bytes.IndexByte(buffer, 0)
-			if index == -1 {
-				index = len(buffer)
-			}
-
-			if _, err = file.Write(buffer[:index]); err != nil {
-				// closing the connection as well
-				receiver.Close()
-				errChan <- err
-				return
-			}
-		}
-	}
-
-	// now getting the response.
-	// it's not unreachable code...
-	_, err = f.getFtpResponse()
-	if err != nil {
-		errChan <- err
-		return
-	}
-
-	doneChan <- struct{}{}
+	// var receiver io.ReadCloser
+	//
+	// if mode == IndMode {
+	// 	mode = f.config.DefaultMode
+	// }
+	//
+	// if mode == ActiveMode {
+	//
+	// 	//opening a listener.
+	// 	listener, err := f.openListener()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	// 	defer listener.Close()
+	//
+	// 	// sending command.
+	// 	if _, err = f.writeCommandAndGetResponse("RETR " + filepathSrc + "\r\n"); err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	// accept connection.
+	// 	receiver, err = listener.Accept()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	// } else if mode == PassiveMode {
+	//
+	// 	addr, err := f.pasvGetAddr()
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	// write command
+	// 	if _, err = f.writeCommandAndGetResponse("RETR " + filepathSrc + "\r\n"); err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	//
+	// 	receiver, err = f.connectToAddr(addr)
+	// 	if err != nil {
+	// 		errChan <- err
+	// 		return
+	// 	}
+	// }
+	//
+	// file, err := os.Create(filepathDest)
+	// if err != nil {
+	// 	errChan <- err
+	// 	return
+	// }
+	//
+	// // starting reading into receiver
+	// buffer := make([]byte, 1024)
+	// loop := true
+	//
+	// // command has been issued, notify on startingChan
+	// startingChan <- struct{}{}
+	//
+	// for loop {
+	//
+	// 	select {
+	//
+	// 	case <-abortChan:
+	// 		receiver.Close()
+	// 		// var response *Response //declaring here just to prevent go vet.
+	// 		response, err := f.writeCommandAndGetResponse("ABOR\r\n")
+	// 		if err != nil {
+	// 			errChan <- err
+	//
+	// 			os.Remove(file.Name()) //skipping the error.
+	// 			return
+	// 		}
+	//
+	// 		// if 426 we have to wait for another response.
+	// 		// if 226 the transfer is complete
+	// 		if response.Code == AbortOk || response.Code == TransferOk {
+	// 			// ok, wait for another.
+	// 			abortResponse, err := f.getFtpResponse()
+	// 			if err != nil {
+	// 				// very bad
+	// 				errChan <- err
+	// 				return
+	// 			} else if abortResponse.Code != TransferOk {
+	// 				errChan <- newUnexpectedCodeError(TransferOk, abortResponse.Code)
+	// 				return
+	// 			}
+	// 		} else {
+	// 			errChan <- newUnexpectedCodeError(AbortOk, response.Code)
+	// 			return
+	// 		}
+	//
+	// 		err = os.Remove(file.Name())
+	// 		if err != nil {
+	// 			errChan <- err
+	// 		} else {
+	// 			doneChan <- struct{}{}
+	// 		}
+	// 		return
+	//
+	// 	default:
+	//
+	// 		_, err = receiver.Read(buffer)
+	// 		if err != nil && err == io.EOF {
+	// 			// I know that it is ugly
+	// 			// but in this way we can skip some if.
+	// 			// EOF means the connection has been closed.
+	// 			loop = false
+	// 			continue // before there was a break but it'll make
+	// 			// exiting just from the select.
+	// 		} else if err != nil {
+	// 			receiver.Close()
+	// 			errChan <- err
+	// 			return
+	// 		}
+	//
+	// 		// so we can skip null bytes that are added
+	// 		/// to fill the buffer size.
+	// 		index := bytes.IndexByte(buffer, 0)
+	// 		if index == -1 {
+	// 			index = len(buffer)
+	// 		}
+	//
+	// 		if _, err = file.Write(buffer[:index]); err != nil {
+	// 			// closing the connection as well
+	// 			receiver.Close()
+	// 			errChan <- err
+	// 			return
+	// 		}
+	// 	}
+	// }
+	//
+	// // now getting the response.
+	// // it's not unreachable code...
+	// _, err = f.getFtpResponse()
+	// if err != nil {
+	// 	errChan <- err
+	// 	return
+	// }
+	//
+	// doneChan <- struct{}{}
 }
